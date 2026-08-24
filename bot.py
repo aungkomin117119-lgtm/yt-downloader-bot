fe6979ad2ed566e31fa9b806fbff3be46a1c0a2b
aungkomin117119-lgtm <aungkomin117119@gmail.com>
2026-08-25 00:36:50 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,10 +28,10 @@ async def download_mp3(update: Update, context: ContextTypes.DEFAULT_TYPE):
    ydl_opts = {
     'format': 'bestaudio/best',
     'cookiefile': 'cookies.txt',
-    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1',
     'extractor_args': {
         'youtube': {
-            'player_client': ['android', 'web'],
+            'player_client': ['ios', 'mweb'],
         }
     },
     'postprocessors': [{
